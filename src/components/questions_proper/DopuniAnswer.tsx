a775f3db1013b5d53569e542e994ca03e221d81f
Salihbasic <mahir.salihbasic@protonmail.com>
2022-09-20 00:04:52 +0200

    Using stateful input fields now

--- src/components/questions_proper/DopuniAnswer.tsx
+++ src/components/questions_proper/DopuniAnswer.tsx
@@ -1,44 +1,15 @@
 import { TextField } from "@mui/material";
 import React from "react";
 
-export default function DopuniAnswer(props: { label: string;
-                                              addFilledField: (label: string) => void;
-                                              removeFilledField: (label: string) => void; 
-                                            }) {
+export default function DopuniAnswer(props: { label: string; handleFilling: (label: string, value: string) => void; }) {
+
+    const label = props.label;
 
     return (
 
         <TextField variant="outlined" 
-                   label={props.label} 
-                   onKeyDown={(event) => {
-
-                        if (event.key === "Enter") {
-
-                            /*
-                                There is some weird React and MUI thing about event target values
-                                that I really don't understand (and can't be bothered to study at
-                                the moment) and their typing.
-
-                                This code *worked* before introducing TypeScript because of some...
-                                magic (???) dynamic voodoo things, so let's just leave it that way.
-                            */
-
-                            // @ts-ignore
-                            const content: string = event.target.value;
-
-                            if (!content.length) {
-
-                                props.addFilledField(props.label)
-
-                            } else {
-
-                                props.removeFilledField(props.label)
-
-                            }
-                            
-                        }
-
-                   }} />
+                   label={label}
+                   onChange={(event) => { props.handleFilling(label, event.target.value) }} />
 
     )
 
